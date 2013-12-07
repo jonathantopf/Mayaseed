ec68b8691b97aa1a384c9e0bbd7c8dc54f89b43d
Jonathan Topf <jonathan@jonathantopf.com>
2013-12-07 19:16:52 +0000

    updated version

--- scripts/ms_commands.py
+++ scripts/ms_commands.py
@@ -38,7 +38,7 @@ import math
 # Constants.
 #--------------------------------------------------------------------------------------------------
 
-MAYASEED_VERSION = '0.5.0'
+MAYASEED_VERSION = '0.6.0'
 MAYASEED_URL = 'https://github.com/jupiter-jazz/Mayaseed'
 APPLESEED_URL = 'http://appleseedhq.net/'
 ROOT_DIRECTORY = os.path.split((os.path.dirname(inspect.getfile(inspect.currentframe()))))[0]
